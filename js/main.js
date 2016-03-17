c77d84d53072a830a86075c51b81f3ff77ca5f7c
Kevin Roark <ker2143@columbia.edu>
2016-03-16 23:41:02 -0400

    infinite image scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
 
 (function() {
+  var contentList = document.querySelector('.content-list');
+  var originalImages = document.querySelectorAll('.content-item');
+
+  var shuffledImageSources = [];
+
+  var nextThreshold = 333;
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > nextThreshold) {
+      nextThreshold += 333;
+      addImagesToBottom();
+    }
+  }, false);
+
+  function addImagesToBottom() {
+    var numberOfImages = Math.ceil(window.innerWidth / 333);
+
+    for (var i = 0 ; i < numberOfImages; i++) {
+      if (shuffledImageSources.length === 0) {
+        refreshShuffledImageSources();
+      }
+
+      var imageSource = shuffledImageSources.pop();
+      addImage(imageSource);
+    }
+  }
+
+  function addImage(src) {
+    var img = document.createElement('img');
+    img.src = src;
+    img.className = 'content-item';
+
+    contentList.appendChild(img);
+  }
+
+  function refreshShuffledImageSources() {
+    for (var i = 0; i < originalImages.length; i++) {
+      shuffledImageSources.push(originalImages[i].src);
+    }
+    shuffledImageSources.sort(function() { return Math.random() - 0.5; });
+  }
 
 })();
